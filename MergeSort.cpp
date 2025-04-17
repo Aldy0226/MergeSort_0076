0ca3b156207d6028f2ff2e612b5b28bcd9538e48
Aldy0226 <putra.jaya.ft24@mail.umy.ac.id>
2025-04-17 18:15:01 +0700

    Step 1 dan 1a

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -3,3 +3,9 @@ using namespace std;
 
 int arr[20], B[20]; // Array utama dan array bantu
 int n; // Jumlah elemen dalam array
+
+void mergesort(int low, int high) {
+    if (low >= high){ // step 1
+        return; // step 1a
+    }
+}
